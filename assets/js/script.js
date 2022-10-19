6f79ee70f3f3b2d19587e10efc6b947e2304904e
Colin Marshall <colin.marshall94@gmail.com>
2022-10-19 09:11:27 -0700

    added reset.css, worked on css, assembled basic html

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+// highscores:
+// if .hsButton clicked, display highscores
+// use local storage to append new game results into highscores{} (JSON.parse, JSON.stringify) and have highscores display the contents of highscores{} by using the index values to line item them.
+
