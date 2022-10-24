aacce0d4c43887bfc4fb6f99811efa6d10261a51
Colin Marshall <colin.marshall94@gmail.com>
2022-10-23 20:01:28 -0700

    changed structure of testQuestions to label each answer option as true or false

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,99 +2,131 @@
 // if .hsButton clicked, display highscores
 // use local storage to append new game results into highscores{} (JSON.parse, JSON.stringify) and have highscores display the contents of highscores{} by using the index values to line item them.
 
-var startButton = document.querySelector('.start')
-var h1 = document.getElementById('h1');
-var p = document.getElementById('p');
-var button = document.getElementById('button');
-var quizAnswersEl = document.getElementById('quizAnswersEl');
+var startButton = document.querySelector(".start");
+var h1 = document.getElementById("h1");
+var p = document.getElementById("p");
 
+var quizAnswersEl = document.getElementById("quizAnswersEl");
 
-questions = ['When a browser displays a page, what is it referencing to build the page?','What is the best way to find out more information about a data type in your code?','How do you create a function in JavaScript?','How does a FOR loop start?','How do you add a comment in a JavaScript file?','What is the correct “if” statements to execute certain code if “x” is equal to 2?','What will <code>Boolean(3<7)</code> return?']
-// console.log(questions.length);
-console.log(questions[0]);
-console.log(questions[6]);
-
-
-var questionRandomIndex = Math.floor(Math.random() * questions.length);
-
-
-//Questions/Answers
-//When a browser displays a page, what is it referencing to build the page?
-//A:  DOM, JavaScript, HTML, CSS
-//What is the best way to find out more information about a data type in your code?
-//A:  Educated guess based on reading the code, use console.log() to display it in DevTools, use window.alert() to display it on the web app, Cry and give up
-//How do you create a function in JavaScript?
-//A: function:myFunction(), function = myFunction(), function myFunction(), myFunction(function)
-//How does a FOR loop start?
-//A: for (i=0; i<=5), for i = i to 5, for (i=0; i<=5, i++), for (i<=5, i++)
-//How do you add a comment in a JavaScript file?
-//A: //Comment, `Comment, <!--Comment-->, >comment
-//What is the correct “if” statements to execute certain code if “x” is equal to 2?
-//A: if(x 2), if(x = 2), if(x == 2)<--correct , if(x != 2 )
-//What will <code>Boolean(3<7)</code> return?
-//A: true, false, NaN, SyntaxError
-
-// POSSIBLE WAY TO SET UP QUIZ QUESTIONS
 var testQuestions = [
-    {
-        //property = value
-        //key value pairs
-        questionText: "123",
-        options: [1,2,3,4],
-        answer: 3
-    },
-    {
-        questionText: "abc",
-        options: [1,2,3,4],
-        answer: 2
-    }
-]
-console.log(testQuestions[0].options[0]);
+  {
+    //property = value
+    //key value pairs
+    questionText:
+      "When a browser displays a page, what is it referencing to build the page?",
+    options: [
+        {option:"DOM",answer:true}, 
+        {option:"JavaScript",answer:false}, 
+        {option:"HTML",answer:false}, 
+        {option:"CSS",answer:false}
+    ],
+  },
+  {
+    //finish setting up questions once linked
+    questionText:
+      "What is the best way to find out more information about a data type in your code?",
+    options: [
+        {option:"Educated guess based on reading the code" ,answer:false},
+        {option:"use console.log() to display it in DevTools" ,answer:true},
+        {option:"use window.alert() to display it on the web app" ,answer:false},
+        {option: "Cry and give up",answer:false},
+    ],
+  },
+  {
+    questionText: "How do you create a function in JavaScript?",
+    options: [
+        {option:"function:myFunction()",answer:false},
+        {option: "function myFunction()" ,answer:false},
+        {option: "myFunction(function)",answer:false},
+        {option:"function = myFunction()" ,answer:true},
+    ],
+    answer: 4,
+  },
+  {
+    questionText: "How does a FOR loop start?",
+    options: [
+        {option:"for (i=0; i<=5)" ,answer:false},
+        {option:"for i = i to 5" ,answer:false},
+        {option:"for (i=0; i<=5, i++)" ,answer:true},
+        {option:"for (i<=5, i++)" ,answer:false},
+    ],
+  },
+  {
+    questionText: "How do you add a comment in a JavaScript file?",
+    options: [
+        {option:"//Comment" ,answer:true},
+        {option:"`Comment" ,answer:false},
+        {option:"<!--Comment-->" ,answer:false},
+        {option:">comment" ,answer:false},
+    ],
+  },
+  {
+    questionText:
+      "What is the correct “if” statements to execute certain code if “x” is equal to 2?",
+    options: [
+        {option:"if(x 2)" ,answer:false},
+        {option:"if(x = 2)" ,answer:false},
+        {option:"if(x == 2)" ,answer:true},
+        {option:"if(x != 2 )" ,answer:false},
+    ],
+  },
+  {
+    questionText: "What will Boolean(3<7) return?",
+    options: [
+        {option:"true" ,answer:true},
+        {option:"false" ,answer:false},
+        {option:"NaN" ,answer:false},
+        {option:"SyntaxError",answer:false},
+    ],
+  },
+];
 
-// function setUpQuestion(questions) {
-//     document.querySelector(".question.text").textContent = question.text;
-// }
+// for randomly selecting questions - will add later
+// var questionRandomIndex = Math.floor(Math.random() * testQuestions.length);
 
-startButton.addEventListener("click", startGame)
+startButton.addEventListener("click", startGame);
 
 function startGame() {
-    // removes content to be replaced with the quiz
-    
-    // h1.remove();
-    // p.remove();
-    // button.remove();
-    questionFormat();
-    //askQuestion();
+  askQuestion();
+  //   questionFormat();
 }
 
 // function askQuestion(){
-    //this function will ask the user questions
-    //upon multiple choice selection, check if answer correct, and store result
-    //
+//this function will ask the user questions
+//upon multiple choice selection, check if answer correct, and store result
+//
 // }
 
-function questionFormat() {
-    //1) randomly select a question
-    console.log(questions[questionRandomIndex]);
-    console.log(h1.textContent);
-    // document.getElementById('h1').innerHTML = 'This is the new question?'
-    h1.textContent = questions[questionRandomIndex];
-    //hide the p tag here
-    //these are hidden, not deleted
-    button.style.display = "none";
-    p.style.display = "none";
-    //hide the start button
-    //insert new content
-    var newButton1 = document.createElement('button');
-    quizAnswersEl.appendChild(newButton1);
-    newButton1.textContent = "test";
-    var newButton2 = document.createElement('button');
-    quizAnswersEl.appendChild(newButton2);
-    newButton2.textContent = "test";
-    var newButton3 = document.createElement('button');
-    quizAnswersEl.appendChild(newButton3);
-    newButton3.textContent = "test";
-    var newButton4 = document.createElement('button');
-    quizAnswersEl.appendChild(newButton4);
-    newButton4.textContent = "test";
+function askQuestion() {
+  var button = document.getElementById("button");
+  var correct = 
+  //1) randomly select a question
+  h1.textContent = testQuestions[0].questionText;
+  //hide the p tag here
+  //these are hidden, not deleted
+  button.style.display = "none";
+  p.style.display = "none";
+  //hide the start button
+  //insert new content
+  var newButton1 = document.createElement("button");
+  quizAnswersEl.appendChild(newButton1);
+  newButton1.className = 0;
+  newButton1.textContent = testQuestions[0].options[0];
+  var newButton2 = document.createElement("button");
+  quizAnswersEl.appendChild(newButton2);
+  newButton2.className = 1;
+  newButton2.textContent = testQuestions[0].options[1];
+  var newButton3 = document.createElement("button");
+  quizAnswersEl.appendChild(newButton3);
+  newButton3.className = 2;
+  newButton3.textContent = testQuestions[0].options[2];
+  var newButton4 = document.createElement("button");
+  quizAnswersEl.appendChild(newButton4);
+  newButton4.className = 3;
+  newButton4.textContent = testQuestions[0].options[3];
+  console.log();
+
+
+  //let testQuestions[0].answer = correct answer, otherwise wrong answer
+  //if button is pressed, testQuestions[i++];, repeat until done
 }
